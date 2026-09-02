42c430f3b0d99f2978d7f299ce69a079f9430fe3
root <root@hermes-web.ronaldleonardo.com>
2026-09-02 05:00:03 +0000

    Auto-backup 2026-09-02 05:00:03

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://growlightdb.ronaldleonardo.com',
+  site: 'https://growlightdb.tabletopharvest.com',
   compressHTML: true,
+  devToolbar: { enabled: false },
   integrations: [sitemap()],
   build: { format: 'file' },
   server: {
     host: '0.0.0.0',
     port: 8081,
-    allowedHosts: ['mia.ronaldleonardo.com', 'growlightdb.ronaldleonardo.com', 'www.tabletopharvest.com', 'tabletopharvest.com'],
+    allowedHosts: ['growlightdb.tabletopharvest.com', 'growlightdb.ronaldleonardo.com', 'www.tabletopharvest.com', 'tabletopharvest.com'],
   },
 });
